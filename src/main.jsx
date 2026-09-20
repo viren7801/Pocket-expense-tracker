7efd1ff3bb0d194c68061d788acb22580fa40d38
viren7801 <127821519+viren7801@users.noreply.github.com>
2026-09-20 09:58:41 +0530

    Register Pocket push service worker

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import LockScreen from "./LockScreen.jsx";
+import { registerPocketServiceWorker } from "./pushNotifications";
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => registerPocketServiceWorker().catch(console.error));
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
